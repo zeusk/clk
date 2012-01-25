5f353cd5a4a3fe8ad15cf12a06ae81ac6a5b0717
Shantanu Gupta <shans95g@gmail.com>
2012-01-25 16:31:47 +0530

    enable support for hardware FPU, needs uncommenting

--- arch/arm/rules.mk
+++ arch/arm/rules.mk
@@ -20,6 +20,7 @@ DEFINES += \
 	ARM_WITH_CACHE=1 \
 	ARM_WITH_L2=1
 CFLAGS += -mcpu=$(ARM_CPU) -mfpu=neon -mfloat-abi=softfp
+#CFLAGS += -march=armv7-a -mfloat-abi=hardfp
 HANDLED_CORE := true
 endif
 ifeq ($(ARM_CPU),arm1136j-s)
